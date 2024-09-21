0d2c38d94e6dbbc65ad0e0c3f8bc8313fb6c30b9
Alia Qureshi <alia2054hotmail.com>
2024-09-21 00:50:00 -0400

    Resolved merge conflicts

--- lab2.js
+++ lab2.js
@@ -6,13 +6,9 @@ const path = require("path")
 const lab_router = require("./router");
 
 app.use(express.urlencoded({ extended: true }));
+
 app.use("/lab2", lab_router)
 
-
-app.get("/calculate", (req, res) => {
-    console.log(req.params);
-    res.send("Enter x or y ");
-})
 app.use("", (req, res) => {
     res.status(404).send("Page Not Found");
 })
